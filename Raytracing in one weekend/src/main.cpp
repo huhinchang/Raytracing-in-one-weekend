bcb63dc3221c407e98f338a81a3854a2832738ee
huhinchang <83922479+huhinchang@users.noreply.github.com>
2022-01-03 22:24:34 -0500

    5-2

--- Raytracing in one weekend/src/main.cpp
+++ Raytracing in one weekend/src/main.cpp
@@ -7,8 +7,22 @@
 // cd "C:\Users\user\Desktop\Other devs\Raytracing in one weekend\Raytracing in one weekend\Debug"
 // "Raytracing in one weekend.exe" > ../Renders/asdf.ppm
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a*c;
+	return (discriminant > 0);
+}
+
 color ray_color(const ray& r)
 {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0); // sphere is red
+
+	// background
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
