4b590340900ec9a4f3837604dae4d1b263733530
huhinchang <83922479+huhinchang@users.noreply.github.com>
2022-01-03 23:22:59 -0500

    up to 7-2

--- Raytracing in one weekend/src/rtweekend.h
+++ Raytracing in one weekend/src/rtweekend.h
@@ -25,6 +25,35 @@ inline double degrees_to_radians(double degrees)
 	return degrees * pi / 180.0;
 }
 
+/*
+#include <random>
+
+inline double random_double() {
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
+*/
+
+inline double random_double()
+{
+	// Returns a random real in [0,1).
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+	// Returns a random real in [min,max).
+	return min + (max - min)*random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
+}
+
 // Common Headers
 
 #include "ray.h"
